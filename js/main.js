23dfd3e1a13de4f55f4954fd82ddd811773e4575
giovannifranchi <giovanni.franchi95@gmail.com>
2023-02-23 17:32:15 +0100

    Feat: interface completed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
+const main = document.querySelector('main');
+
+
 for(let i = 1; i <= 100; i++){
+    const box = document.createElement('div');
+    box.classList.add('box');
+    main.append(box);
+    const text = document.createElement('h2');
+    box.append(text);
     if(i % 3 === 0 && i % 5 === 0){
         console.log('FizzBuzz');
+        text.innerHTML = 'FizzBuzz';
+        box.classList.add('red');
     }else if(i % 3 === 0){
         console.log('Fizz');
+        text.innerHTML = 'Fizz';
+        box.classList.add('green');
     }else if(i % 5 === 0){
         console.log('Buzz');
+        text.innerHTML = 'Buzz';
+        box.classList.add('yellow');
     }else {
         console.log(i);
+        text.innerHTML = i;
+        box.classList.add('blue');
     }
 }
+
+
