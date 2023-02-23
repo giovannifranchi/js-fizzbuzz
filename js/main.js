6cea60de67146e688a6198f1871cce02a21467fa
giovannifranchi <giovanni.franchi95@gmail.com>
2023-02-23 17:37:13 +0100

    Fix: google font and text align of h3

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ for(let i = 1; i <= 100; i++){
     const box = document.createElement('div');
     box.classList.add('box');
     main.append(box);
-    const text = document.createElement('h2');
+    const text = document.createElement('h3');
     box.append(text);
     if(i % 3 === 0 && i % 5 === 0){
         console.log('FizzBuzz');
